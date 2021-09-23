cf6a4ef67ca739809ae2937c119405fa1c579421
YuJeong Jeong <yu2jeong@naver.com>
2021-09-23 23:27:42 +0900

    add 35. Search Insert Position

---
+++ leetcode/35. Search Insert Position/35_search_insert_posiiton.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int SearchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.Length-1;
+        int mid = 0;
+        while(left <= right)
+        {
+            mid = left + (right - left ) / 2;
+            if(nums[mid] == target)
+            {
+                return mid;
+            }
+            else if(nums[mid] < target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+        return right + 1;
+    }
+}
